47d466aa1d5f050c547b3ca5d23da7f8b2e33027
MD Bayzed Meer <bayzed.meer@brainstation-23.com>
2024-06-06 13:57:13 +0600

    feat : add configuration before bootstrap application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TestService } from './services/test.service';
 import { CommonModule } from '@angular/common';
+import { ConfigService } from './services/config.service';
 
 @Component({
   selector: 'app-root',
@@ -12,11 +13,17 @@ import { CommonModule } from '@angular/common';
 })
 export class AppComponent implements OnInit {
   title = 'Multi-Tenant-with-CI-CD';
+  api: any;
 
-  constructor(private testService: TestService) {}
+  constructor(
+    private testService: TestService,
+    private config: ConfigService,
+  ) {}
 
   ngOnInit(): void {
     this.testService.fetch().subscribe();
+    this.api = this.config.api();
+    console.log(this.api);
   }
 
   showError(): void {
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,17 @@
-import { ApplicationConfig, ErrorHandler, provideZoneChangeDetection } from '@angular/core';
+import {
+  APP_INITIALIZER,
+  ApplicationConfig,
+  ErrorHandler,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { HttpErrorInterceptor } from './interceptors/http-error.interceptor';
 import { GlobalErrorHandler } from './services/global-error-handler.service';
+import { take } from 'rxjs';
+import { ConfigService } from './services/config.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -14,6 +21,17 @@ export const appConfig: ApplicationConfig = {
     {
       provide: ErrorHandler,
       useClass: GlobalErrorHandler,
+    },
+    {
+      provide: APP_INITIALIZER,
+      multi: true,
+      useFactory: (config: ConfigService) => {
+        return () => {
+          config.fetchEndpoints();
+          return config.api$.pipe(take(1));
+        };
+      },
+      deps: [ConfigService],
     },
     {
       provide: HTTP_INTERCEPTORS,
