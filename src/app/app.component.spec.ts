f6061fd0364cc28aa0d5009672de041b972229f4
MD Bayzed Meer <bayzed.meer@brainstation-23.com>
2024-06-06 18:02:19 +0600

    feat : add ngx-http-error-handler library published and integrated

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,5 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { provideHttpClient } from '@angular/common/http';
 
 describe('AppComponent', () => {
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,20 +8,15 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
-import { HttpErrorInterceptor } from './interceptors/http-error.interceptor';
-import { GlobalErrorHandler } from './services/global-error-handler.service';
 import { take } from 'rxjs';
 import { ConfigService } from './services/config.service';
+import { NgxHttpErrorHandler, NgxHttpErrorInterceptor } from 'ngx-http-error-handler';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideHttpClient(withInterceptorsFromDi()),
     provideRouter(routes),
-    {
-      provide: ErrorHandler,
-      useClass: GlobalErrorHandler,
-    },
     {
       provide: APP_INITIALIZER,
       multi: true,
@@ -35,8 +30,12 @@ export const appConfig: ApplicationConfig = {
     },
     {
       provide: HTTP_INTERCEPTORS,
-      useClass: HttpErrorInterceptor,
+      useClass: NgxHttpErrorInterceptor,
       multi: true,
+    },
+    {
+      provide: ErrorHandler,
+      useClass: NgxHttpErrorHandler,
     },
   ],
 };
